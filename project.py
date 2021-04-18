c2450cbc758cfd146d4223e7b8a8efa49b43eeba
JiviteshJamuda <jivitesh2006@gmail.com>
2021-04-18 20:23:56 +0530

    final

--- project.py
+++ project.py
@@ -2,11 +2,8 @@ import pandas as pd
 import csv
 
 df = pd.read_csv('merged_data.csv')
-print(df.head())
-# print(df.shape)
+df = df.drop([0], axis=0)
 del df['luminosity']
 df.dropna()
 # print(df.shape)
-# print(list(df))
-
 df.to_csv('final.csv')
