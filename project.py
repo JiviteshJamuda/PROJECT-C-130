b29bee193b17744de602ea01b1c68482ab0bd7c9
JiviteshJamuda <jivitesh2006@gmail.com>
2021-04-18 22:49:18 +0530

    FINAL

--- project.py
+++ project.py
@@ -2,8 +2,10 @@ import pandas as pd
 import csv
 
 df = pd.read_csv('merged_data.csv')
-df = df.drop([0], axis=0)
+print(df.shape)
+df.drop(df.index[(df["mass"] == "?")],axis=0,inplace=True)
+df.drop(df.index[(df["radius"] == "?")],axis=0,inplace=True)
+print(df.shape)
 del df['luminosity']
-df.dropna()
-# print(df.shape)
+print(df.shape)
 df.to_csv('final.csv')
